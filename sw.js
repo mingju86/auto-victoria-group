8a51bd6dd7c638c6d4ed3bb62fccdab7a100b718
mingju86 <mingju86@hotmail.com>
2026-08-11 09:15:50 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const C='auto-victoria-v1';const A=['./','./index.html','./manifest.webmanifest','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
